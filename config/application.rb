1892e25dcf9404822bf1c19c1fd14d299fd98945
Catherine Oxley <catherine.oxley@live.co.uk>
2017-03-08 22:44:03 +0000

    finished page

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,11 @@ module CatherineOxleyCv
       generate.assets false
     end
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
